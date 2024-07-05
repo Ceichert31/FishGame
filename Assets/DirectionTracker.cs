834ca918ad81ee91f5adc3f1810610c16a2fc3f5
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2024-07-05 00:38:55 -0500

    Lure Boss Changes
    -Hooks now move opposite to the direction the boss is moving

---
+++ Assets/DirectionTracker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionTracker : MonoBehaviour
+{
+    Transform playerTransform;
+    Vector3 previousPosition;
+    Vector3 playerDirection;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        playerTransform = transform.parent;
+        previousPosition = playerTransform.position;
+    }
+
+    private void Update()
+    {
+        playerDirection = playerTransform.position - previousPosition;
+        previousPosition = playerTransform.position;
+    }
+
+    public Vector3 PlayerDirection
+    { 
+        get { return new Vector3(playerDirection.z, playerDirection.x, 0).normalized; }
+    }
+}
---
+++ Assets/Scripts/Boss AI/LureBoss/HookController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DirectionTracker))]
+public class HookController : MonoBehaviour
+{
+    [Header("Hook List")]
+    [SerializeField] List<Transform> hooks = new List<Transform>();
+
+    [Header("Hook Limits")]
+    float maxYRot = 75;
+    float maxXRot = 30;
+    float rotateAmmount = 100f;
+    float rotateBackAmmount = 10f;
+
+    [SerializeField] float tempHookX;
+    [SerializeField] float tempHookY;
+    Vector3 tempDirection;
+
+    DirectionTracker directionTracker;
+    Vector3 rotateDirection;
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        directionTracker = GetComponent<DirectionTracker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateDirection = directionTracker.PlayerDirection;
+
+        if(rotateDirection.magnitude == 0)
+        {
+            for (int i = 0; i < hooks.Count; i++)
+            {
+                hooks[i].localRotation = Quaternion.Lerp(hooks[i].localRotation, Quaternion.identity, Time.deltaTime * rotateBackAmmount);
+            }
+            return;
+        }
+
+        for (int i = 0; i < hooks.Count; i++)
+        {
+            // Apply rotation
+            hooks[i].localEulerAngles -= rotateDirection * rotateAmmount * Time.deltaTime;
+
+            // Normalize angles to the range of -180 to 180 degrees
+            Vector3 eulerAngles = hooks[i].localEulerAngles;
+            eulerAngles.x = NormalizeAngle(eulerAngles.x);
+            eulerAngles.y = NormalizeAngle(eulerAngles.y);
+
+            // Clamp angles within the specified range
+            float tempHookX = Mathf.Clamp(eulerAngles.x, -maxXRot, maxXRot);
+            float tempHookY = Mathf.Clamp(eulerAngles.y, -maxYRot, maxYRot);
+
+            Debug.Log(tempHookY);
+
+            // Apply the clamped angles back to the hook
+            hooks[i].localEulerAngles = new Vector3(tempHookX, tempHookY, eulerAngles.z);
+        }
+    }
+
+    /// <summary>
+    /// Makes an angle normalized between -180 and 180 as to not cause overflow errors when clamping angles
+    /// </summary>
+    /// <param name="angle"></param>
+    /// <returns></returns>
+    float NormalizeAngle(float angle)
+    {
+        angle = angle % 360;
+        if (angle > 180) angle -= 360;
+        else if (angle < -180) angle += 360;
+        return angle;
+    }
+}
