00c256516e65543e20e5b7f14cd5544db502f05d
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2024-08-14 19:50:57 -0400

    Started work on SFX
    - Added audio pitcher
    - Added Footstep audio pitcher for player
    - Added new footstep SFX

---
+++ Assets/Scripts/Player/FootstepAudioPitcher.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepAudioPitcher : MonoBehaviour
+{
+    [Header("Scriptable Object Reference")]
+    [SerializeField] AudioPitcherSO audioPitcher;
+
+
+}
