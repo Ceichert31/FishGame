3ef8d21406cf256ce53b7016d56ae90732ed5ca2
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2024-09-04 23:13:55 -0400

    Cooper Push
    -Lure boss now sheens when physically parried

---
+++ Assets/Scripts/SheenController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enum to mimic boolean behavior (False = 0, True = 1)
+enum Bools
+{
+    False = 0,
+    True = 1,
+}
+
+public class SheenController : MonoBehaviour
+{
+    [SerializeField] Material parrySheen;
+    [SerializeField] Texture bossTexture;
+    const float DEFAULTVALUE = -0.31f;        // Starting value for the sheen
+    const float ENDVALUE = 0.15f;             // Final value for the sheen
+    [SerializeField] float multiplier = 1; 
+
+    private void Awake()
+    {
+        parrySheen.SetTexture("_BossTexture", bossTexture); // Assign boss texture
+    }
+
+    void Start()
+    {
+        parrySheen.SetFloat("_Control", DEFAULTVALUE); // Initialize sheen control
+    }
+
+    /// <summary>
+    /// Callable from a Float event listener to start the shee effect
+    /// </summary>
+    /// <param name="ctx"></param>
+    public void StartSheen(FloatEvent ctx)
+    {
+        StopAllCoroutines();
+        ParrySheenSet(Caster(Bools.True));
+        StartCoroutine(CommenceSheen());
+    }
+
+    /// <summary>
+    /// Moves the value of the shader to induce a sheen effect
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator CommenceSheen()
+    {
+        float currentValue = DEFAULTVALUE;
+        while (currentValue < ENDVALUE)
+        {
+            currentValue += Time.deltaTime * multiplier; 
+            parrySheen.SetFloat("_Control", currentValue);
+            yield return null;
+        }
+        ParrySheenSet(Caster(Bools.False));
+    }
+
+    private void OnDisable()
+    {
+        ParrySheenSet(Caster(Bools.False));
+    }
+
+    /// <summary>
+    /// Method for repeatedly setting material values
+    /// </summary>
+    /// <param name="trueFalse"></param>
+    void ParrySheenSet(int trueFalse)
+    {
+        parrySheen.SetFloat("_Boolean", trueFalse);
+        parrySheen.SetFloat("_Control", DEFAULTVALUE);
+    }
+
+    int Caster(Bools value)
+    {
+        return (int)value;
+    }
+}
