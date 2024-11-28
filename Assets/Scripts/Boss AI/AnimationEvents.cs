094a618706a4ef759ead1097469cae471db8f7e4
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2024-11-27 20:32:03 -0500

    Cooper Push
    -Added new animation event based behaviors
    -Added documentation for said system

--- Assets/Scripts/Boss AI/AnimationEvents.cs
+++ Assets/Scripts/Boss AI/AnimationEvents.cs
@@ -11,8 +11,14 @@ public enum ParticleTypes
 
 public abstract class AnimationEvents : MonoBehaviour
 {
+    public delegate void ActiveBehavior();
+    public ActiveBehavior activeBehavior;
+
     [SerializeField] Transform modelsTransform;
     [SerializeField] AudioPitcherSO attackAudio;
+
+    IBossWalkBehavior bossWalkBehavior;
+    IBossLookAtPlayer bossLookAtPlayer;
 
 
     [Tooltip("Order: StaggerParticles, ParryParticles")]
@@ -46,6 +52,13 @@ public abstract class AnimationEvents : MonoBehaviour
         bossAnimator = GetComponent<Animator>();
         bossPosture = modelsTransform.GetComponent<BossPosture>();
         bossHealth = bossPosture.GetComponent<BossHealth>();
+
+        bossWalkBehavior = transform.GetChild(0).GetComponent<IBossWalkBehavior>();
+        bossLookAtPlayer = transform.GetChild(0).GetComponent<IBossLookAtPlayer>();
+
+        //temp add so delegate is never null
+        activeBehavior += () => { };
+
     }
 
     /// <summary>
@@ -106,4 +119,51 @@ public abstract class AnimationEvents : MonoBehaviour
     {
         bossHealth.ProcedeWithDeath();
     }
+
+    /// <summary>
+    /// 0: Boss Starts Looking At Player
+    /// 1: Boss Stops Looking At The Player
+    /// 2: Boss Starts Moving Toward The Player
+    /// 3: Boss Stops Moving Toward The Player
+    /// </summary>
+    /// <param name="behavior"></param>
+    public virtual void UpdateBossActiveBehavior(int behavior)
+    {
+        switch (behavior)
+        {
+            case 0:
+                activeBehavior += bossLookAtPlayer.LookAtPlayer;
+                break;
+            case 1:
+                activeBehavior -= bossLookAtPlayer.LookAtPlayer;
+                break;
+            case 2:
+                activeBehavior += bossWalkBehavior.MoveBehavior;
+                break;
+            case 3:
+                activeBehavior -= bossWalkBehavior.MoveBehavior;
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        activeBehavior();
+    }
 }
+/* -How To Use Active Behavior-
+ * 
+ * Purpose: To be able to tell the boss when to do certain time based actions during animations
+ * 
+ * Coding Standereds: Assigning a behavior and De-Assigning a behvaior should be done in subsequent order, 
+ *                    and should also be added above the method in a comment
+ * 
+ * Variables:
+ * delegate ActiveBehvaior: public delegate
+ * ActiveBehavior activeBehavior: instance of delegate used to assign active methods to. Called in update to execute behaviors
+ * 
+ * Methods:
+ * virutal UpdateBossActiveBehavior(int behavior): Virtual method allowing us to pass in numbers to dictate what behavior we want to execute.
+ *                                                 Method is virtual to allow child classes to make their own behavior based on specific needs.
+ * 
+ */
